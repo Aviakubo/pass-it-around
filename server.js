1ac9c85ee0a306c6196d9d6914d80efb04a86456
David Yakubov <dyakubov@Davids-MacBook-Air.local>
2021-09-22 10:42:49 -0400

    not sure what's not working

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 console.log(express);
+const port = 3000;
 
 const app = express();
 
+var bottlesNum = 99;
+
 app.get('/', (req, res) => {
-    res.send('99 bottles of beer on the wall');
+    res.send(`${bottlesNum} bottles of beer on the wall
+    <a href="${bottlesNum-1}">Take one down, pass it around</a>`);
 });
 
-app.listen(3000);
+app.get('/:numberOfBottles', (req, res) => {
+    var remain = parseInt(bottlesNum - req.params.numberOfBottles);
+    res.send(`${bottlesNum} bottles of beer on the wall <a href="${remain-1}">Take one down, pass it around</a>`)
+})
+
+app.listen(port, () => {
+    console.log(`listening on port`, port)
+});
